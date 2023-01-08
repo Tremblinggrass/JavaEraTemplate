f9f8cfd0156010136b0a7aa305ae84c559a72973
Tremblinggrass <gy5831249@qq.com>
2023-01-09 01:37:33 +0800

    添加项目

---
+++ src/main/java/com/template/javaeratemplate/JavaEraTemplateApplication.java
@@ -0,0 +1,16 @@
+package com.template.javaeratemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 文字游戏引擎，启动类
+ */
+@SpringBootApplication
+public class JavaEraTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEraTemplateApplication.class, args);
+    }
+
+}
